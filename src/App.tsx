7ee0e5368803a9d5a5574f0ee2d3b2ece1667202
Florjan96 <florianshiqerukaj@yahoo.com>
2022-09-23 17:24:10 +0200

    Started from beggining. Created state in app.tsx and i will work at app.tsx then i will break into components

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,83 @@
-import { useState } from 'react'
-import './App.css'
-import { Header } from './assets/components/Header'
-import { Main } from './assets/components/Main'
-import state from './data/State'
+import { useState } from "react";
+import "./App.css";
+
 
 function App() {
-  const [items, setitems] = useState(state)
+  const [items, setitems] = useState([
+    {
+      id: 1,
+      sender: `Zoom`,
+      title: `Cloud Recording - Nicolas Marcora's Personal Meeting Room is now available`,
+      starred: false,
+      read: true
+    },
+    {
+      id: 2,
+      sender: `Zoom`,
+      title: `Kushtrim has joined your Personal Meeting Room`,
+      starred: false,
+      read: false
+    },
+    {
+      id: 3,
+      sender: `Notion`,
+      title: `1 update in Hoxton`,
+      starred: true,
+      read: true
+    },
+    {
+      id: 4,
+      sender: `The Calendly Team`,
+      title: `Use more than one calendar?`,
+      starred: false,
+      read: false
+    }])
 
   return (
     <div className="App">
-   <Header/>
+      <header id="store">
+        <h1>Grocero</h1>
+        <ul className="item-list store--item-list">
+          <li>
+            <div className="store--item-icon">
+              <img src="assets/icons/001-beetroot.svg" alt="beetroot" />
+            </div>
+            <button>Add to cart</button>
+          </li>
+        </ul>
+      </header>
 
-   <Main/>
+      <main id="cart">
+        <h2>Your Cart</h2>
+
+        <div className="cart--item-list-container">
+          <ul className="item-list cart--item-list">
+            <li>
+              <img
+                className="cart--item-icon"
+                src="assets/icons/001-beetroot.svg"
+                alt="beetroot"
+              />
+              <p>beetroot</p>
+              <button className="quantity-btn remove-btn center">-</button>
+              <span className="quantity-text center">1</span>
+              <button className="quantity-btn add-btn center">+</button>
+            </li>
+          </ul>
+        </div>
+
+        <div className="total-section">
+          <div>
+            <h3>Total</h3>
+          </div>
+
+          <div>
+            <span className="total-number">£0.00</span>
+          </div>
+        </div>
+      </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
